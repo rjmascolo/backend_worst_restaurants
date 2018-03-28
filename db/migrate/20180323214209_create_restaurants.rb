b000cd3d2b21bcc4dfefa929c9b8be912d17e3de
github username <rjmascolo@gmail.com>
2018-03-28 14:39:52 -0400

    fixed data

---
+++ db/migrate/20180323214209_create_restaurants.rb
@@ -0,0 +1,15 @@
+class CreateRestaurants < ActiveRecord::Migration[5.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :cuisine
+      t.string :zipcode
+      t.string :building
+      t.string :street
+      t.string :phone_number
+      t.decimal :lat, null:true
+      t.decimal :long, null:true
+      
+    end
+  end
+end
